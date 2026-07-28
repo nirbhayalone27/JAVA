6fa39c50ed71d2a3fa48bf56828dab60820a4d63
Nirbhay Alone <hellonirbhay27@gmail.com>
2026-07-28 23:37:02 +0530

    Add files via upload

---
+++ variable.java
@@ -0,0 +1,13 @@
+class Hello
+{
+
+    public static void main(String a[])
+
+    { 
+
+        System.out.println(4 + 5);
+        System.out.println(3 + 4);
+
+    }
+
+}
---
+++ variable2.java
@@ -0,0 +1,11 @@
+class Variable 
+{
+
+    public static void main(String a[])
+
+    {
+        int num1 = 1;
+        int num2 = 4;
+        System.out.println(num1 + num2 );
+    }
+}
