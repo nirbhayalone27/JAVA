5a40915ef0b1c2f992e80db44bc2dc24d3f97f40
Nirbhay Alone <hellonirbhay27@gmail.com>
2026-07-29 23:21:01 +0530

    Add files via upload

---
+++ multiplication.java
@@ -0,0 +1,19 @@
+class Table
+{
+    public static void main(String a[])
+    {
+       
+            System.out.println("Name : Nirbhay Alone");
+            System.out.println("Roll No: 94");
+        
+        int num = 5;
+
+        for(int i = 1; i <= 10; i++)
+        {
+            
+            System.out.println(num * i);
+            
+
+        }
+    }
+}
